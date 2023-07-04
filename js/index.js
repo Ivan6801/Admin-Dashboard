88acca272f1e0a4f173333238c847b769b574e04
Ivan Gonzalez <hal308366@gmail.com>
2023-07-04 13:15:41 -0700

    [ADD]:  Filling Table with Orders using JavaScript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+const orderList = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LARVENDER KF102",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Decline",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
+
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
@@ -19,4 +52,18 @@ themeToggler.addEventListener("click", () => {
 
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+// fill orders in table
+orderList.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `<tr>
+  <td>${order.productName}</td>
+  <td>85631</td>
+  <td>Due</td>
+  <td class="warning">Pending</td>
+  <td class="primary">Details</td>
+</tr>`;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
 });
